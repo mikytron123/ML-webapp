d3663dfe1766207fba6087bc11077e3f6a59b245
Miky <vyasmiky@gmail.com>
2024-08-16 15:05:53 -0400

    changed frontend to use generated class

---
+++ Blazor/Shared/Utils.cs
@@ -0,0 +1,19 @@
+using System.Runtime.Serialization;
+
+namespace Blazor.Shared
+{
+    public static class Utility
+    {
+        public static string GetEnumMemberAttrValue(Type enumType, object enumVal)
+        {
+            var memInfo = enumType.GetMember(enumVal.ToString());
+            var attr = memInfo[0].GetCustomAttributes(false).OfType<EnumMemberAttribute>().FirstOrDefault();
+            if (attr != null)
+            {
+                return attr.Value;
+            }
+
+            return null;
+        }
+    }
+}
